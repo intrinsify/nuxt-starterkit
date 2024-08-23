ae498eeb1b05a95965ba0365214c05e61278b44e
Christopher H. Stappert <post@chstappert.de>
2024-08-23 11:12:45 +0200

    add basic modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/image",
+    "@nuxt/fonts",
+    "@nuxt/scripts",
+    "@nuxt/icon",
+    "@nuxt/eslint"
+  ],
+  eslint: {
+    config: {
+      stylistic: true // <---
+    }
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true }
 })
